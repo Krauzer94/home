ad1b5dcb1d68a6ce1a272514e9caa6bba76204ac
Krauzer94 <13894059+Krauzer94@users.noreply.github.com>
2024-03-03 21:20:40 -0300

    FFmpeg filter to keep original bitrate

--- Videos/.video-editing.sh
+++ Videos/.video-editing.sh
@@ -10,12 +10,12 @@ done
 
 # Cropp to fill
 for f in *.mp4; do
-    ffmpeg -i "$f" -vf "crop=1280:720:0:40" "cropped_$f" -y && rm "$f";
+    ffmpeg -i "$f" -vf "crop=1280:720:0:40" -b:v 6000k "cropped_$f" -y && rm "$f";
 done
 
 # Rescale to 1080p
 for f in *.mp4; do
-    ffmpeg -i "$f" -vf "scale=1920:1080" "scaled_$f" -y && rm "$f";
+    ffmpeg -i "$f" -vf "scale=1920:1080" -b:v 6000k "scaled_$f" -y && rm "$f";
 done
 
 # Trim and fade
@@ -23,7 +23,7 @@ for f in *.mp4; do
     `# Trim 20 seconds` ffmpeg -ss 10 -i $f -t 20 \
     `# Video fade 1sec` -vf 'fade=t=in:st=0:d=1,fade=t=out:st=19:d=1' \
     `# Audio fade 1sec` -af 'afade=t=in:st=0:d=1,afade=t=out:st=19:d=1' \
-    `# Output file` 'edited_'$f -y
+    `# Output file` -b:v 6000k 'edited_'$f -y
     rm $f;
 done
 
