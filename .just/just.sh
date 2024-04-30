a26d56700d7a5a5581b7cb1c497cd96d4a2e32ed
Krauzer94 <13894059+Krauzer94@users.noreply.github.com>
2024-04-30 11:48:03 -0300

    Path handling for new user juser folder

--- .just/just.sh
+++ .just/just.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | bash -s -- --to ~/
+curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | bash -s -- --to ~/bin
